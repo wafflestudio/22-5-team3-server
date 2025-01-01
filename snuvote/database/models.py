45b229c219fd91c3e83734c7bbff8bcb0beb94f5
odumag99 <odumag99@gmail.com>
2025-01-01 19:07:25 +0900

    User DB에 name, college 추가

--- snuvote/database/models.py
+++ snuvote/database/models.py
@@ -1,7 +1,7 @@
 from __future__ import annotations
 from typing import List, Optional
 
-from sqlalchemy import BigInteger, String, ForeignKey
+from sqlalchemy import BigInteger, String, ForeignKey, Integer
 from sqlalchemy.orm import Mapped, mapped_column, relationship
 from snuvote.database.common import Base
 
@@ -9,7 +9,9 @@ from snuvote.database.common import Base
 class User(Base):
     __tablename__ = "user"
 
-    id: Mapped[int] = mapped_column(BigInteger, primary_key=True)
+    id: Mapped[int] = mapped_column(Integer, primary_key=True)
     userid: Mapped[str] = mapped_column(String(20), unique=True, index=True)
     email: Mapped[str] = mapped_column(String(100), unique=True, index=True)
     password: Mapped[str] = mapped_column(String(20))
+    name: Mapped[str] = mapped_column(String(20))
+    college: Mapped[int] = mapped_column(Integer)
