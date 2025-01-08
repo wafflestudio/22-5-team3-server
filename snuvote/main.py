a92a0e63c367ae5d1d9f7c5976a1d9d609a9f0fd
odumag99 <odumag99@gmail.com>
2025-01-08 21:56:50 +0900

    Error Handler가 정상 작동하지 않고 Internal Server Error를 반환하는 문제 해결

--- snuvote/main.py
+++ snuvote/main.py
@@ -11,8 +11,8 @@ app.include_router(api_router, prefix="/api")
 
 
 @app.exception_handler(RequestValidationError)
-def validation_exception_handler(request: Request, exc: RequestValidationError):
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
     for error in exc.errors():
         if isinstance(error, dict) and error.get("type", None) == "missing":
             raise MissingRequiredFieldError()
-    return request_validation_exception_handler(request, exc)
+    return await request_validation_exception_handler(request, exc)
