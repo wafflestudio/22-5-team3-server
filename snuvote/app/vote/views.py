5be28b892684c5a7535699d40aab9db9f5a02466
morecleverer <kimdk0824@naver.com>
2025-01-26 18:09:20 +0900

    ongoing_list 엔드포인트 제거

--- snuvote/app/vote/views.py
+++ snuvote/app/vote/views.py
@@ -48,21 +48,7 @@ def create_vote(
 
     return get_vote(vote.id, user, vote_service)
 
-# 진행 중인 투표 리스트 조회
-#아직 프론트에서 list API 변경이 안되어서 남겨둠
-#함수는 지울 예정
-@vote_router.get("/ongoing_list", status_code=HTTP_200_OK)
-def get_ongoing_list(
-    user: Annotated[User, Depends(login_with_access_token)],
-    vote_service: Annotated[VoteService, Depends()],
-    start_cursor: datetime|None = None
-):
-    results, has_next, next_cursor = vote_service.get_ongoing_list(start_cursor)
-    return OnGoingVotesListResponse(
-        votes_list = [ VotesListInfoResponse.from_vote_user(vote, user, participant_count) for vote, participant_count in results ],
-        has_next = has_next,
-        next_cursor = next_cursor
-    )
+
 
 
 # 완료된/진행중인/hot 투표글 조회
