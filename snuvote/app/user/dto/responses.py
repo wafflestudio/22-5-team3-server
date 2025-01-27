76472ab2f9038fa980ec9f259c09f14cad8ef2a1
morecleverer <kimdk0824@naver.com>
2025-01-27 18:10:13 +0900

    유저 정보에 계정연동 정보도 포함

--- snuvote/app/user/dto/responses.py
+++ snuvote/app/user/dto/responses.py
@@ -14,12 +14,16 @@ class UserInfoResponse(BaseModel):
     userid: str
     email: str
     college: int
+    is_naver_user: bool
+    is_kakao_user: bool
 
     @staticmethod
-    def from_user(user: User) -> "UserInfoResponse":
+    def from_user(user: User) -> "UserInfoResponse":        
         return UserInfoResponse(
             name = user.name,
             userid = user.userid,
             email = user.email,
-            college = user.college
+            college = user.college,
+            is_naver_user = user.naver_user != None,
+            is_kakao_user = user.kakao_user != None
         )
