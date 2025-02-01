9b44a10bb6212c688dbb4471413af2774c55e7da
odumag99 <odumag99@gmail.com>
2025-02-01 13:01:13 +0900

    alembic async화

--- snuvote/database/settings.py
+++ snuvote/database/settings.py
@@ -10,16 +10,9 @@ class DatabaseSettings(BaseSettings):
     user: str = ""
     password: str = ""
     database: str = ""
-    alembic_prefix: str | None = None
-
-    def __init__(self, prefix: str|None = None):
-        super().__init__()
-        self.alembic_prefix = prefix
 
     @property
     def url(self) -> str:
-        if self.alembic_prefix == "alembic":
-            return f"mysql+pymysql://admin:{self.password}@{self.host}:{self.port}/{self.database}"
         return f"mysql+aiomysql://admin:{self.password}@{self.host}:{self.port}/{self.database}"
 
     model_config = SettingsConfigDict(
@@ -31,4 +24,3 @@ class DatabaseSettings(BaseSettings):
 
 
 DB_SETTINGS = DatabaseSettings()
-DB_ALEMBIC_SETTINGS = DatabaseSettings('alembic')
